a068c77813d292db58ae1f7416341687d01dddfb
Abhishek Chatterjee <t-abhishek2.chatterjee@paytmmoney.com>
2023-03-10 17:47:41 +0530

    final

--- mongo_atlas/src/main/java/com/test/mongo_atlas/config/MongoConfig.java
+++ mongo_atlas/src/main/java/com/test/mongo_atlas/config/MongoConfig.java
@@ -4,18 +4,23 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
 @Configuration
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
     @Bean
     @Primary
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/tasks"));
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoUri));
     }
 
 }
